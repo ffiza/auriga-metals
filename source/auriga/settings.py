ba3550305d77768a4a8a0027249111dcd8c82c23
ffiza <fgiza@protonmail.com>
2022-09-24 19:25:42 -0300

    Add attributes to the Settings class.

--- source/auriga/settings.py
+++ source/auriga/settings.py
@@ -2,16 +2,38 @@
 
 class Settings:
     """
-    A class used to manage the general configurations of this project.
+    A class to manage several configuration variables.
 
     Attributes
     ----------
-    first_snapshot : int
-        The snapshot in which to start the analysis.
     repo_name : str
-        The name of the repository of this project.
+        The name of the respository for this project.
+    first_snap : int
+        The first snapshot to consider in all analysis.
+    processes : int
+        A (maybe used) variable that has the amount of processes to use when
+        calling Pool() from the multiprocessing package.
+    galaxies : list
+        A list of all galaxies.
+    reruns : list
+        A list of the rerun galaxies.
+    window_length : int
+        The window length used in the Savitzky-Golay filter for smoothing.
+    polyorder : ing
+        The order of the polynomial used in the Savitzky-Golay filter.
+
+    Methods
+    -------
+    snapshot_path(galaxy, rerun, resolution)
+        Return the path to the snapshots.
     """
 
     def __init__(self) -> None:
-        self.first_snapshot = 15
         self.repo_name = 'auriga-metals'
+        self.first_snap = 15
+        self.galaxies = [i for i in range(1, 31)]
+        self.reruns = [5, 6, 9, 13, 17, 23, 24, 26, 28]
+
+        # Smoothing configuration.
+        self.window_length = 9
+        self.polyorder = 1
