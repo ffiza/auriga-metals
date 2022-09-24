0f46ab5ebe3e7878ad8c85056025c40347ddd69d
ffiza <fgiza@protonmail.com>
2022-09-24 19:26:53 -0300

    Add a file with several useful functions.

---
+++ source/utils/support.py
@@ -0,0 +1,82 @@
+import numpy as np
+import time
+from typing import Callable
+
+
+def find_indices(a: np.array, b: np.array,
+                 invalid_specifier: int = -1) -> np.array:
+    """"
+    Returns an array with the indices of the elements of b
+    in a. If an element of b is not in a, it returns
+    invalid_specifier for that element.
+
+    Parameters
+    ----------
+    a : np.array
+        An array in which to search.
+    b : np.array
+        An array that contains the values to be searched for.
+    invalid_specifier : optional
+        A value to use in case the element is not found.
+
+    Returns
+    -------
+    np.array
+        An array with the indices.
+    """
+
+    sidx = a.argsort()
+    idx = np.searchsorted(a, b, sorter=sidx)
+    idx[idx == len(a)] = 0
+    idx0 = sidx[idx]
+    return np.where(a[idx0] == b, idx0, invalid_specifier)
+
+
+def snapshot_path(galaxy: int, rerun: bool, resolution: int) -> str:
+    """
+    This method creates the path to the snapshot files based on the
+    simulation.
+
+    Parameters
+    ----------
+    galaxy : int
+        The number of the halo.
+    rerun : bool
+        A bool to indicate if this is a original run or a rerun.
+    resolution : int
+        The resolution level of the simulation.
+    """
+
+    if rerun:
+        dir_name = 'Original'
+    else:
+        dir_name = 'RerunsHighFreqStellarSnaps'
+
+    snapshot_path = f'/virgotng/mpa/Auriga/level{resolution}/' + \
+        f'{dir_name}/halo_{galaxy}/output/'
+    return snapshot_path
+
+
+def timer(method: Callable) -> Callable:
+    """
+    A decorator to monitor the time it takes to run a function.
+
+    Parameters
+    ----------
+    method : func
+        A method to decorate.
+
+    Returns
+    -------
+    wrapper : Callable
+        A wrapper.
+    """
+    def wrapper(*args, **kw):
+        start_time = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+
+        print(endTime - start_time, 'ms')
+        return result
+
+    return wrapper
