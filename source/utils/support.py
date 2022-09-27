03df48db6788d38ab71393c55b81e3aa0e0c8e33
ffiza <fgiza@protonmail.com>
2022-09-27 13:47:55 -0300

    Remove path creation method.

--- source/utils/support.py
+++ source/utils/support.py
@@ -33,49 +33,6 @@ def find_indices(a: np.array, b: np.array,
     return np.where(a[idx0] == b, idx0, invalid_specifier)
 
 
-def snapshot_path(galaxy: int, rerun: bool, resolution: int) -> str:
-    """
-    This method creates the path to the snapshot files based on the
-    simulation.
-
-    Parameters
-    ----------
-    galaxy : int
-        The number of the halo.
-    rerun : bool
-        A bool to indicate if this is a original run or a rerun.
-    resolution : int
-        The resolution level of the simulation.
-
-    Returns
-    -------
-    str
-        The path to the snapshot files.
-    """
-
-    if os.uname()[1] == 'virgo':
-        if rerun:
-            dir_name = 'RerunsHighFreqStellarSnaps'
-        else:
-            dir_name = 'Original'
-
-        snapshot_path = f'/virgotng/mpa/Auriga/level{resolution}/' + \
-            f'{dir_name}/halo_{galaxy}/output/'
-        return snapshot_path
-    elif os.uname()[1] == 'neuromancer':
-        if rerun:
-            rerun_text = '_rerun'
-        else:
-            rerun_text = ''
-
-        if (galaxy != 6) or (resolution != 4):
-            raise Exception('Only halo 6 with resolution 4 is stored locally.')
-
-        snapshot_path = '/media/federico/Elements1/Simulations/' + \
-            f'au{galaxy}{rerun_text}/'
-        return snapshot_path
-
-
 def timer(method: Callable) -> Callable:
     """
     A decorator to monitor the time it takes to run a function.
