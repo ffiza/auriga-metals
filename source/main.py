654801bef3b61663bf29bc8d48bac069fc09eaaf
ffiza <fgiza@protonmail.com>
2022-09-27 14:25:46 -0300

    Remove main.py.

--- source/main.py
+++
@@ -1,9 +0,0 @@
-from source.auriga.snapshot import Snapshot
-from source.auriga.settings import Settings
-from source.auriga.cosmology import Cosmology
-
-s = Snapshot()
-settings = Settings()
-cosmology = Cosmology()
-
-x = [1, 2]
