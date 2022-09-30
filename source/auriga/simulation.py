c1693d783cb3df4a6af7ccc80bf546d534008a6a
Federico Iza <fgiza@protonmail.com>
2022-09-30 23:40:49 +0200

    Add the number of snapshots to the Simulation class.

--- source/auriga/simulation.py
+++ source/auriga/simulation.py
@@ -8,6 +8,10 @@ class Simulation:
 
     Attributes
     ----------
+    _rerun : bool
+        The type of simulation.
+    _resolution : int
+        The resolution of the simulation.
     snapshot_numbers : np.array
         An array with the snapshot numbers of each snapshot.
     times : np.array
@@ -17,23 +21,34 @@ class Simulation:
         An array with the redshift of each snapshot.
     expansion_factors : np.array
         An array with the expansion factor of each snapshot.
+    n_snapshots : int
+        The number of snapshots in this simulation.
     """
 
-    def __init__(self, rerun=False):
+    def __init__(self, rerun: bool, resolution: int):
         """
         Parameters
         ----------
         rerun : bool
             A bool indicating if the simulation is the original run or
             the rerun.
+        resolution : int
+            The resolution of the simulation.
         """
 
-        self.type = 'Rerun' if rerun is True else 'Original'
-        with open('data/simulation.json') as f:
+        self._rerun = rerun
+        self._resolution = resolution
+
+        self._type = 'Rerun' if self._rerun else 'Original'
+
+        with open(f'data/level{self._resolution}/simulation.json') as f:
             data = json.load(f)
 
-            self.snapshot_numbers = np.array(data[self.type]['SnapshotNumber'])
-            self.times = np.array(data[self.type]['Time_Gyr'])
-            self.redshifts = np.array(data[self.type]['Redshift'])
+            self.snapshot_numbers = np.array(
+                data[self._type]['SnapshotNumber'])
+            self.times = np.array(data[self._type]['Time_Gyr'])
+            self.redshifts = np.array(data[self._type]['Redshift'])
             self.expansion_factors = np.array(
-                data[self.type]['ExpansionFactor'])
+                data[self._type]['ExpansionFactor'])
+
+        self.n_snapshots = 252 if self._rerun else 128
