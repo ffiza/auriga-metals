0b8b4650d19b4ba21c1f8ba97a3da660924fa46f
ffiza <fgiza@protonmail.com>
2023-02-06 15:12:36 -0300

    Add type hints.

--- source/auriga/physics.py
+++ source/auriga/physics.py
@@ -25,23 +25,24 @@ class Physics:
     atomic_numbers : dict
         A dictionary with the atomic number of each metal.
     """
+
     def __init__(self) -> None:
-        self.solar_metallicity = 0.0127
-        self.gravitational_constant = 4.3E-3
-        self.proton_mass = 1.67262192369
-        self.solar_mass = 1.98847
-        self.critical_temperature = 2E4
-        self.star_forming_density = 0.13
+        self.solar_metallicity: float = 0.0127
+        self.gravitational_constant: float = 4.3E-3
+        self.proton_mass: float = 1.67262192369
+        self.solar_mass: float = 1.98847
+        self.critical_temperature: float = 2E4
+        self.star_forming_density: float = 0.13
 
         # Metals
-        self.metals = ['H', 'He', 'C', 'N', 'O', 'Ne', 'Mg', 'Si', 'Fe']
+        self.metals: list = ['H', 'He', 'C', 'N', 'O', 'Ne', 'Mg', 'Si', 'Fe']
         # Asplund+2009 (Table 5)
-        self.solar_abundances = {'H': 12, 'He': 10.98, 'C': 8.47,
-                                 'N': 7.87, 'O': 8.73, 'Ne': 7.97,
-                                 'Mg': 7.64, 'Si': 7.55, 'Fe': 7.54}
-        self.atomic_numbers = {'H': 1, 'He': 4, 'C': 12,
-                               'N': 14, 'O': 16, 'Ne': 20,
-                               'Mg': 24, 'Si': 28, 'Fe': 56}
+        self.solar_abundances: dict = {'H': 12, 'He': 10.98, 'C': 8.47,
+                                       'N': 7.87, 'O': 8.73, 'Ne': 7.97,
+                                       'Mg': 7.64, 'Si': 7.55, 'Fe': 7.54}
+        self.atomic_numbers: dict = {'H': 1, 'He': 4, 'C': 12,
+                                     'N': 14, 'O': 16, 'Ne': 20,
+                                     'Mg': 24, 'Si': 28, 'Fe': 56}
 
 # 3He, 12C, 24Mg, 16O, 56Fe, 28Si, H, 14N, 20Ne, 32S, 40Ca, 62Zn, 56N
 # Woosley & Woosley
