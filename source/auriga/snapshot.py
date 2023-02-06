2d4cc8144be47b2580ceb84cfb0d91e311390356
ffiza <fgiza@protonmail.com>
2023-02-06 15:06:11 -0300

    Improve formatting.

--- source/auriga/snapshot.py
+++ source/auriga/snapshot.py
@@ -88,36 +88,41 @@ class Snapshot:
         self._paths = Paths(self.galaxy, self.rerun, self.resolution)
         self.snapnum = snapnum
 
-        sf = gadget_readsnap(snapshot=self.snapnum,
-                             snappath=self._paths.snapshots,
-                             lazy_load=True,
-                             cosmological=False,
-                             applytransformationfacs=False)
-        sb = load_subfind(id=self.snapnum,
-                          dir=self._paths.snapshots,
-                          cosmological=False)
+        sf = gadget_readsnap(
+            snapshot=self.snapnum,
+            snappath=self._paths.snapshots,
+            lazy_load=True,
+            cosmological=False,
+            applytransformationfacs=False,
+        )
+        sb = load_subfind(
+            id=self.snapnum, dir=self._paths.snapshots, cosmological=False
+        )
         sf.calc_sf_indizes(sf=sb)
 
         self.rotation_matrix = np.loadtxt(
-            f'{self._paths.data}'
-            + 'rotation_matrices.csv')[self.snapnum].reshape((3, 3))
+            f"{self._paths.data}" + "rotation_matrices.csv"
+        )[self.snapnum].reshape((3, 3))
 
-        self.subhalo_vel = np.loadtxt(
-            f'{self._paths.data}subhalo_vels.csv')[self.snapnum]
+        self.subhalo_vel = np.loadtxt(f"{self._paths.data}subhalo_vels.csv")[
+            self.snapnum
+        ]
 
         # Set halo/subhalo indices.
-        main_obj_df = pd.read_csv(f'{self._paths.data}main_object_idxs.csv')
+        main_obj_df = pd.read_csv(f"{self._paths.data}main_object_idxs.csv")
         self._halo_idx = main_obj_df.MainHaloIDX.iloc[snapnum]
         self._subhalo_idx = main_obj_df.MainSubhaloIDX.iloc[snapnum]
 
-        subhalo_grouptab_idx = sb.data['ffsh'][self._halo_idx] \
+        subhalo_grouptab_idx = sb.data["ffsh"][self._halo_idx] \
             + self._subhalo_idx
 
-        pos = (self.rotation_matrix @ (sf.pos
-                                       - sb.data['spos'][subhalo_grouptab_idx]
-                                       / sf.hubbleparam).T).T * 1E3
+        pos = (
+            self.rotation_matrix @ (sf.pos
+                                    - sb.data["spos"][subhalo_grouptab_idx]
+                                    / sf.hubbleparam).T
+        ).T * 1e3
         vel = (self.rotation_matrix @ (sf.vel * np.sqrt(sf.time)
-                                       - self.subhalo_vel).T).T
+               - self.subhalo_vel).T).T
 
         # Snapshot properties.
         self.redshift = sf.redshift
@@ -126,23 +131,23 @@ class Snapshot:
         self.time = cosmology.redshift_to_time(self.redshift)
 
         # General particle properties.
-        self.df['xCoordinates'] = pos[:, 0]  # ckpc
-        self.df['yCoordinates'] = pos[:, 1]  # ckpc
-        self.df['zCoordinates'] = pos[:, 2]  # ckpc
-        self.df['xVelocities'] = vel[:, 0]  # km/s
-        self.df['yVelocities'] = vel[:, 1]  # km/s
-        self.df['zVelocities'] = vel[:, 2]  # km/s
-        self.df['PartTypes'] = sf.type
-        self.df['ParticleIDs'] = sf.id
-        self.df['Potential'] = sf.pot / self.expansion_factor  # (km/s)^2
-        self.df['Masses'] = sf.mass * 1E10  # Msun
-        self.df['Halo'] = sf.halo
-        self.df['Subhalo'] = sf.subhalo
+        self.df["xCoordinates"] = pos[:, 0]  # ckpc
+        self.df["yCoordinates"] = pos[:, 1]  # ckpc
+        self.df["zCoordinates"] = pos[:, 2]  # ckpc
+        self.df["xVelocities"] = vel[:, 0]  # km/s
+        self.df["yVelocities"] = vel[:, 1]  # km/s
+        self.df["zVelocities"] = vel[:, 2]  # km/s
+        self.df["PartTypes"] = sf.type
+        self.df["ParticleIDs"] = sf.id
+        self.df["Potential"] = sf.pot / self.expansion_factor  # (km/s)^2
+        self.df["Masses"] = sf.mass * 1e10  # Msun
+        self.df["Halo"] = sf.halo
+        self.df["Subhalo"] = sf.subhalo
 
         # Stellar formation time.
         formation_time = np.nan * np.ones(sf.mass.shape[0])
         formation_time[sf.type == 4] = sf.age
-        self.df['StellarFormationTime'] = formation_time
+        self.df["StellarFormationTime"] = formation_time
 
     def keep_only_feats(self, feats: list) -> None:
         """
@@ -171,50 +176,60 @@ class Snapshot:
         # Check if all particles are loaded in the data frame.
         for include_type in [0, 1, 2, 3, 4, 5]:
             if include_type not in self.df.PartTypes:
-                raise ValueError(f'Type {include_type} particles/cells not'
-                                 f'loaded.')
+                raise ValueError(
+                    f"Type {include_type} particles/cells not" f"loaded.")
 
         # Check if spherical coordinates were calculated.
-        if 'rCoordinates' not in self.df.columns.values:
+        if "rCoordinates" not in self.df.columns.values:
             self.calc_extra_coordinates()
 
-        pos = self.df[['xCoordinates', 'yCoordinates',
-                       'zCoordinates']].to_numpy()
-        vel = self.df[['xVelocity', 'yVelocity', 'zVelocity']].to_numpy()
+        pos = self.df[["xCoordinates", "yCoordinates",
+                       "zCoordinates"]].to_numpy()
+        vel = self.df[["xVelocity", "yVelocity", "zVelocity"]].to_numpy()
         jz = np.cross(pos, vel)[:, 2] * self.expansion_factor  # kpc km/s
         del pos, vel
 
-        is_galaxy = (self.df.Halo == self._halo_idx) \
-            & (self.df.Subhalo == self._subhalo_idx)
+        is_galaxy = (self.df.Halo == self._halo_idx) & (
+            self.df.Subhalo == self._subhalo_idx
+        )
         is_star = (self.df.PartTypes == 4) & (self.df.StellarFormationTime > 0)
 
         bins = np.asarray(
-            [0] + list(np.sort(self.df.rCoordinates[is_galaxy & is_star])))
+            [0] + list(np.sort(self.df.rCoordinates[is_galaxy & is_star]))
+        )
         ii = self.df.rCoordinates[is_galaxy & is_star].argsort().argsort()
         enclosed_mass = np.add.accumulate(
-            np.histogram(
-                self.df.rCoordinates,
-                weights=self.df.Masses, bins=bins)[0])[ii]
+            np.histogram(self.df.rCoordinates, weights=self.df.Masses,
+                         bins=bins)[0]
+        )[ii]
         del bins, ii
 
         with warnings.catch_warnings():
             # Ignore RuntimeWarnings due to division by zero.
-            warnings.simplefilter('ignore', RuntimeWarning)
-            jc = self.df.rCoordinates[is_galaxy & is_star] \
-                * self.expansion_factor \
-                * np.sqrt(physics.gravitational_constant * enclosed_mass
-                          / (1E3 * self.df.rCoordinates[is_galaxy & is_star]
-                             * self.expansion_factor))  # kpc km/s
+            warnings.simplefilter("ignore", RuntimeWarning)
+            jc = (
+                self.df.rCoordinates[is_galaxy & is_star]
+                * self.expansion_factor
+                * np.sqrt(
+                    physics.gravitational_constant
+                    * enclosed_mass
+                    / (
+                        1e3
+                        * self.df.rCoordinates[is_galaxy & is_star]
+                        * self.expansion_factor
+                    )
+                )
+            )  # kpc km/s
         del enclosed_mass
 
         # Calculate circularity parameter.
         eps = np.nan * np.ones(self.df.Masses.shape[0])
         with warnings.catch_warnings():
             # Ignore RuntimeWarnings due to division by zero.
-            warnings.simplefilter('ignore', RuntimeWarning)
+            warnings.simplefilter("ignore", RuntimeWarning)
             eps[is_galaxy & is_star] = jz[is_galaxy & is_star] / jc
         del jz, jc, is_galaxy, is_star
-        self.df['Circularity'] = eps
+        self.df["Circularity"] = eps
 
     def keep_only_halo(self, halo: int = None, subhalo: int = None) -> None:
         """
@@ -266,10 +281,10 @@ class Snapshot:
         spherical coordinates.
         """
 
-        pos = self.df[['xCoordinates', 'yCoordinates',
-                       'zCoordinates']].to_numpy()
-        self.df['rxyCoordinates'] = np.linalg.norm(pos[:, 0:2], axis=1)
-        self.df['rCoordinates'] = np.linalg.norm(pos, axis=1)
+        pos = self.df[["xCoordinates", "yCoordinates",
+                       "zCoordinates"]].to_numpy()
+        self.df["rxyCoordinates"] = np.linalg.norm(pos[:, 0:2], axis=1)
+        self.df["rCoordinates"] = np.linalg.norm(pos, axis=1)
 
     def calc_birth_snapnum(self) -> None:
         """
@@ -286,4 +301,4 @@ class Snapshot:
             stellar_birth_snapnum[(new_exp_fact_diff < 0)
                                   & (exp_fact_diff > 0)] = i
             exp_fact_diff = new_exp_fact_diff
-        self.df['StellarFormationSnapshotNumber'] = stellar_birth_snapnum
+        self.df["StellarFormationSnapshotNumber"] = stellar_birth_snapnum
