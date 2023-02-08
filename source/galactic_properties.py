bd7aaadfc7c0bb71c1ab7a13fb0e60eacf18b3f3
ffiza <fgiza@protonmail.com>
2023-02-08 10:57:41 -0300

    Add method to generate plot and also save the props of object (0, 0) to data frame.

--- source/galactic_properties.py
+++ source/galactic_properties.py
@@ -1,6 +1,8 @@
 from multiprocessing import Pool
 from support import make_snapshot_number
 from sys import stdout
+import matplotlib.pyplot as plt
+import pandas as pd
 import os
 os.environ["MKL_NUM_THREADS"] = "1"  # Limits threads in Numpy
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
@@ -8,10 +10,11 @@ os.environ["OMP_NUM_THREADS"] = "1"
 import numpy as np
 
 from cosmology import Cosmology
-from pylib import gadget, gadget_subfind
+from loadmodules import gadget_readsnap, load_subfind
 from settings import Settings
 from paths import Paths
 from support import timer, create_or_load_dataframe
+from images import add_redshift, figure_setup
 
 
 class GalacticPropertiesAnalysis:
@@ -88,12 +91,12 @@ class GalacticPropertiesAnalysis:
         settings = Settings()
         cosmology = Cosmology()
 
-        sf = gadget.gadget_readsnap(snapshot=snapshot_number,
-                                    snappath=self._paths.snapshots,
-                                    onlyHeader=True,
-                                    lazy_load=True,
-                                    cosmological=False,
-                                    applytransformationfacs=False)
+        sf = gadget_readsnap(snapshot=snapshot_number,
+                             snappath=self._paths.snapshots,
+                             onlyHeader=True,
+                             lazy_load=True,
+                             cosmological=False,
+                             applytransformationfacs=False)
         time = cosmology.redshift_to_time(sf.redshift)
         lookback_time = cosmology.redshift_to_lookback_time(sf.redshift)
         expansion_factor = sf.time
@@ -101,9 +104,9 @@ class GalacticPropertiesAnalysis:
         del sf
 
         if snapshot_number >= settings.first_snap:
-            sb = gadget_subfind.load_subfind(id=snapshot_number,
-                                             dir=self._paths.snapshots,
-                                             cosmological=False)
+            sb = load_subfind(id=snapshot_number,
+                              dir=self._paths.snapshots,
+                              cosmological=False)
 
             halo_idx = self._df["MainHaloIdx"].loc[snapshot_number]
             subhalo_idx = self._df["MainSubhaloIdx"].loc[snapshot_number]
@@ -113,13 +116,20 @@ class GalacticPropertiesAnalysis:
                 / sb.hubbleparam  # ckpc
             virial_mass = sb.data['fmc2'][subhalo_grouptab_idx] \
                 / sb.hubbleparam  # 1E10 Msun
+
+            # For reference, also consider the virial properties of the (0, 0)
+            # object (same units)
+            virial_radius_00 = sb.data['frc2'][0] * 1E3 / sb.hubbleparam
+            virial_mass_00 = sb.data['fmc2'][0] / sb.hubbleparam
+
             del sb
         else:
             virial_radius = np.nan
             virial_mass = np.nan
 
         return (snapshot_number, time, lookback_time, redshift,
-                expansion_factor, virial_radius, virial_mass)
+                expansion_factor, virial_radius, virial_mass,
+                virial_radius_00, virial_mass_00)
 
     @timer
     def analyze_galaxy(self) -> None:
@@ -141,6 +151,8 @@ class GalacticPropertiesAnalysis:
         self._df["ExpansionFactor"] = np.round(data[:, 4], 7)
         self._df["VirialRadius_ckpc"] = np.round(data[:, 5], 7)
         self._df["VirialMass_1E10Msun"] = np.round(data[:, 6], 7)
+        self._df["VirialRadius00_ckpc"] = np.round(data[:, 7], 7)
+        self._df["VirialMass00_1E10Msun"] = np.round(data[:, 8], 7)
 
         self._save_data()
 
@@ -169,5 +181,63 @@ def main() -> None:
             run_analysis(galaxy=galaxy, rerun=True, resolution=4)
 
 
+def plot_virial_radius(savefig: bool) -> None:
+    """
+    This method creates a plot to visualize the virial radius of each
+    galaxy.
+    """
+
+    settings = Settings()
+
+    fig = plt.figure(figsize=settings.big_fig_size)
+    gs = fig.add_gridspec(nrows=settings.big_fig_nrows,
+                          ncols=settings.big_fig_ncols,
+                          hspace=settings.big_fig_hspace,
+                          wspace=settings.big_fig_wspace)
+    axs = gs.subplots(sharex=True, sharey=True)
+
+    for ax_idx, ax in enumerate(axs.flat):
+        ax.label_outer()
+        ax.grid(True, ls='-', lw=settings.big_fig_grid_lw, c='silver')
+        ax.tick_params(which='both', direction="in")
+        ax.set_xlim(0, 14)
+        ax.set_ylim(0, 400)
+        ax.set_xticks([2, 4, 6, 8, 10, 12, 14])
+        ax.set_yticks([100, 200, 300, 400])
+
+        galaxy = ax_idx + 1
+
+        paths = Paths(galaxy, False, 4)
+        df = pd.read_csv(f"{paths.data}temporal_data.csv")
+        ax.plot(df["Time_Gyr"], df["VirialRadius_ckpc"], c='k',
+                lw=settings.big_fig_line_lw, zorder=10)
+
+        if galaxy in settings.reruns:
+            paths = Paths(galaxy, True, 4)
+            df = pd.read_csv(f"{paths.data}temporal_data.csv")
+            ax.plot(df["Time_Gyr"], df["VirialRadius_ckpc"], c='tab:red',
+                    lw=settings.big_fig_line_lw, ls="--", zorder=11)
+
+        add_redshift(ax)
+        ax.text(0.95, 0.95, f'Au{galaxy}', size=6,
+                ha='right', va='top',
+                transform=ax.transAxes,
+                bbox={"facecolor": "silver", "edgecolor": "white",
+                      "pad": .2, 'boxstyle': 'round', 'lw': 1})
+
+        if ax.get_subplotspec().is_first_col():
+            ax.set_ylabel(r"$R_{200}$ [ckpc]")
+        if ax.get_subplotspec().is_last_row():
+            ax.set_xlabel('Time [Gyr]')
+
+    if savefig:
+        for extension in settings.figure_extensions:
+            fig.savefig(f"images/level4/virial_radius.{extension}")
+        plt.close(fig)
+    else:
+        plt.show()
+
+
 if __name__ == "__main__":
-    main()
+    figure_setup()
+    plot_virial_radius(savefig=True)
