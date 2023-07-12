449b924db7a8c59f53e188e66082be7aec222365
Federico Iza <fgiza@protonmail.com>
2023-07-12 16:30:48 +0200

    Add tests.

--- tests/test_find_indices.py
+++ tests/test_find_indices.py
@@ -29,6 +29,24 @@ class FindIndicesTest(unittest.TestCase):
         results = np.array([2, 2, 2])
         self.assertTrue((indices == results).all())
 
+    def test_4(self):
+        a = np.array([1, 2, 3, 4, 5, 6, 7])
+        b = np.array([2, 7, 8, 3, 2, 19, 22, 14])
+        indices = find_indices(a=a, b=b)
+        self.assertTrue(len(b) == len(indices))
+
+    def test_5(self):
+        a = np.array([1, 2, 3, 4, 5, 6, 7])
+        b = np.array([2, 7, 8, 3, 2, 19, 22, 14])
+        indices = find_indices(a=a, b=b)
+        self.assertTrue((a[indices[indices >= 0]] == b[indices >= 0]).all())
+
+    def test_6(self):
+        a = np.array([-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7])
+        b = np.array([2, 43, -2, -27, 8, 3, 2, 19, 22, 14])
+        indices = find_indices(a=a, b=b)
+        self.assertTrue((a[indices[indices >= 0]] == b[indices >= 0]).all())
+
 
 if __name__ == '__main__':
     unittest.main()
