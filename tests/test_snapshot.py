9c434699e08914a4b0e7a47211ff12c92bb66e92
Federico Iza <fgiza@protonmail.com>
2023-09-14 20:03:42 +0200

    Update tests.

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -14,33 +14,29 @@ class SnapshotTests(unittest.TestCase):
 
     def test_stellar_formation_snapshot(self):
         s = Snapshot(simulation="au6_or_l4_s100", loadonlytype=[4])
-        stellar_formation_snapshot = \
-            s._calculate_stellar_formation_snapshot()
-        self.assertTrue(stellar_formation_snapshot[0] == 81)
-        self.assertTrue(stellar_formation_snapshot[1] == 53)
-        self.assertTrue(stellar_formation_snapshot[2] == 56)
-        self.assertTrue(stellar_formation_snapshot[3] == 49)
-        self.assertTrue(stellar_formation_snapshot[4] == 39)
-        self.assertTrue(stellar_formation_snapshot[5] == 59)
-        self.assertTrue(stellar_formation_snapshot[8] == 60)
+        s.calculate_stellar_formation_snapshot()
+        self.assertTrue(s.stellar_formation_snapshot[0] == 81)
+        self.assertTrue(s.stellar_formation_snapshot[1] == 53)
+        self.assertTrue(s.stellar_formation_snapshot[2] == 56)
+        self.assertTrue(s.stellar_formation_snapshot[3] == 49)
+        self.assertTrue(s.stellar_formation_snapshot[4] == 39)
+        self.assertTrue(s.stellar_formation_snapshot[5] == 59)
+        self.assertTrue(s.stellar_formation_snapshot[8] == 60)
 
     def test_stellar_formation_snapshot_max(self):
         s = Snapshot(simulation="au6_or_l4_s100", loadonlytype=[4])
-        stellar_formation_snapshot = \
-            s._calculate_stellar_formation_snapshot()
-        self.assertTrue(stellar_formation_snapshot.max() <= 100)
+        s.calculate_stellar_formation_snapshot()
+        self.assertTrue(s.stellar_formation_snapshot.max() <= 100)
 
         s = Snapshot(simulation="au6_or_l4_s99", loadonlytype=[4])
-        stellar_formation_snapshot = \
-            s._calculate_stellar_formation_snapshot()
-        self.assertTrue(stellar_formation_snapshot.max() <= 99)
+        s.calculate_stellar_formation_snapshot()
+        self.assertTrue(s.stellar_formation_snapshot.max() <= 99)
 
     def test_stellar_formation_snapshot_for_non_stellar_part(self):
         s = Snapshot(simulation="au6_or_l4_s100", loadonlytype=[0, 4])
-        stellar_formation_snapshot = \
-            s._calculate_stellar_formation_snapshot()
-        self.assertTrue(stellar_formation_snapshot[s.type == 0].max() == -1)
-        self.assertTrue(stellar_formation_snapshot[s.type == 0].min() == -1)
+        s.calculate_stellar_formation_snapshot()
+        self.assertTrue(s.stellar_formation_snapshot[s.type == 0].max() == -1)
+        self.assertTrue(s.stellar_formation_snapshot[s.type == 0].min() == -1)
 
 
 if __name__ == '__main__':
