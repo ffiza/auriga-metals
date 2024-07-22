0259a85bdb0f1534401d61fa4bbb382fb0d204a6
Federico Iza <fgiza@protonmail.com>
2024-07-22 18:30:57 +0200

    Add new scripts, save data and output figure.

---
+++ scripts/data/age_metallicity.py
@@ -0,0 +1,146 @@
+"""
+File:           age_metallicity.py
+Author:         Federico Iza
+Description:    This script reads data from a snapshot, calculated binned
+                statistics over the stellar ages and saves the results by
+                galaxy.
+Usage:          `python scripts/data/age_metalllicity.py --simulation
+                au6_or_l4_s127 --config 02`
+"""
+import numpy as np
+from scipy.stats import binned_statistic
+import pandas as pd
+import yaml
+import argparse
+
+from auriga.snapshot import Snapshot
+from auriga.settings import Settings
+from auriga.paths import Paths
+from auriga.parser import parse
+from auriga.mathematics import mad
+
+
+def read_data(simulation: str, config: dict) -> pd.DataFrame:
+    """
+    This method returns data related to this analysis.
+
+    Parameters
+    ----------
+    simulation : str
+        The simulation to consider.
+    config : dict
+        A dictionary with the configuration values.
+
+    Returns
+    -------
+    pd.DataFrame
+        The properties in a Pandas DataFrame.
+    """
+    s = Snapshot(simulation=simulation, loadonlytype=[0, 1, 2, 3, 4, 5])
+    s.add_stellar_age()
+    s.add_metal_abundance(of="Fe", to='H')
+    s.add_circularity()
+    s.tag_particles_by_region(
+        disc_std_circ=config["DISC_STD_CIRC"],
+        disc_min_circ=config["DISC_MIN_CIRC"],
+        cold_disc_delta_circ=config["COLD_DISC_DELTA_CIRC"],
+        bulge_max_specific_energy=config["BULGE_MAX_SPECIFIC_ENERGY"])
+
+    is_real_star = (s.type == 4) & (s.stellar_formation_time > 0)
+    is_main_obj = (s.halo == s.halo_idx) & (s.subhalo == s.subhalo_idx)
+
+    props = {
+        "StellarAge_Gyr": s.stellar_age[is_real_star & is_main_obj],
+        "[Fe/H]": s.metal_abundance["Fe/H"][is_real_star & is_main_obj],
+        "ComponentTag": s.region_tag[is_real_star & is_main_obj]}
+
+    df = pd.DataFrame(props)
+    df[~np.isfinite(df)] = np.nan
+    df.dropna(inplace=True)
+    return df
+
+
+def get_stats_of_df(df: pd.DataFrame, config: dict) -> pd.DataFrame:
+    """
+    This method returns the stats calculated on `df`.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data.
+    config : dict
+        A dictionary with the configuration values.
+
+    Returns
+    -------
+    pd.DataFrame
+        The statistical properties of `df`.
+    """
+    settings = Settings()
+
+    binned_median, bin_edges, _ = binned_statistic(
+        x=df["StellarAge_Gyr"],
+        values=df["[Fe/H]"],
+        statistic=np.nanmedian,
+        bins=config["AGE_METALLICITY_RELATION"]["N_BINS_AGE"],
+        range=(config["AGE_METALLICITY_RELATION"]["AGE_MIN"],
+               config["AGE_METALLICITY_RELATION"]["AGE_MAX"]))
+    binned_mad = binned_statistic(
+        x=df["StellarAge_Gyr"],
+        values=df["[Fe/H]"],
+        statistic=mad,
+        bins=config["AGE_METALLICITY_RELATION"]["N_BINS_AGE"],
+        range=(config["AGE_METALLICITY_RELATION"]["AGE_MIN"],
+               config["AGE_METALLICITY_RELATION"]["AGE_MAX"]))[0]
+    bin_centers = bin_edges[1:] - np.diff(bin_edges)[0] / 2
+
+    stats = {"[Fe/H]_Median": binned_median,
+             "[Fe/H]_MAD": binned_mad,
+             "StellarAge_BinCenters_Gyr": bin_centers}
+    
+    # Add median por each component
+    for i in range(len(settings.components)):
+        binned_median, _, _ = binned_statistic(
+            x=df["StellarAge_Gyr"][df["ComponentTag"] == i],
+            values=df["[Fe/H]"][df["ComponentTag"] == i],
+            statistic=np.nanmedian,
+            bins=config["AGE_METALLICITY_RELATION"]["N_BINS_AGE"],
+            range=(config["AGE_METALLICITY_RELATION"]["AGE_MIN"],
+                   config["AGE_METALLICITY_RELATION"]["AGE_MAX"]))
+        binned_mad, _, _ = binned_statistic(
+            x=df["StellarAge_Gyr"][df["ComponentTag"] == i],
+            values=df["[Fe/H]"][df["ComponentTag"] == i],
+            statistic=mad,
+            bins=config["AGE_METALLICITY_RELATION"]["N_BINS_AGE"],
+            range=(config["AGE_METALLICITY_RELATION"]["AGE_MIN"],
+                   config["AGE_METALLICITY_RELATION"]["AGE_MAX"]))
+        stats[f"[Fe/H]_Median_{settings.components[i]}"] = binned_median
+        stats[f"[Fe/H]_MAD_{settings.components[i]}"] = binned_mad
+
+    stats = pd.DataFrame(stats)
+    return stats
+
+
+def main():
+    # Get arguments from user
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--simulation", type=str, required=True)
+    parser.add_argument("--config", type=str, required=True)
+    args = parser.parse_args()
+
+    # Load configuration file
+    config = yaml.safe_load(open(f"configs/{args.config}.yml"))
+
+    # Run the analysis
+    data = read_data(simulation=args.simulation, config=config)
+    stats = get_stats_of_df(df=data, config=config)
+    stats = stats.round(config["AGE_METALLICITY_RELATION"]["DECIMALS"])
+
+    # Save data
+    galaxy, rerun, resolution, _ = parse(args.simulation)
+    paths = Paths(galaxy, rerun, resolution)
+    stats.to_csv(f"{paths.results}/age_metallicity{config['FILE_SUFFIX']}.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/visualization/age_metallicity.py
@@ -0,0 +1,114 @@
+"""
+File:           age_metallicity.py
+Author:         Federico Iza
+Description:    This script creates a plot of the age-metallicity relation
+                showing all galaxies in the sample.
+Usage:          `python scripts/visualization/age_metalllicity.py --config 20`
+"""
+from matplotlib import pyplot as plt
+import pandas as pd
+import yaml
+import argparse
+
+from auriga.images import figure_setup
+from auriga.settings import Settings
+from auriga.parser import parse
+from auriga.paths import Paths
+
+
+def make_plot(config: dict):
+    settings = Settings()
+    sample = [f"au{i}_or_l4_s127" for i in settings.groups["Included"]]
+    facts = [1, 2, -1, -2]
+    lss = ["--", ":", '--', ':']
+
+    fig = plt.figure(figsize=(7.4, 2.0))
+    gs = fig.add_gridspec(nrows=1, ncols=5, hspace=0.0, wspace=0.0)
+    axs = gs.subplots(sharex=True, sharey=True)
+
+    for ax in axs.flat:
+        ax.set_xlim(0, 14)
+        ax.set_xticks([2, 4, 6, 8, 10, 12])
+        ax.set_xlabel("Stellar Age [Gyr]")
+
+        ax.set_ylim(-2, 1.5)
+        ax.set_yticks([-1.5, -1, -0.5, 0, 0.5, 1])
+        ax.set_ylabel("[Fe/H]")
+
+        ax.tick_params(which='both', direction="in")
+        ax.label_outer()
+
+    for simulation in sample:
+        galaxy, rerun, resolution, _ = parse(simulation)
+        paths = Paths(galaxy, rerun, resolution)
+        data = pd.read_csv(
+            f"{paths.results}age_metallicity{config['FILE_SUFFIX']}.csv")
+        color = "black" if galaxy == 6 else "silver"
+        zorder = 11 if galaxy == 6 else 10
+
+        axs[0].plot(data["StellarAge_BinCenters_Gyr"], data["[Fe/H]_Median"],
+                    ls="-", c=color, zorder=zorder, lw=1.0)
+        if galaxy == 6:
+            for i in range(len(facts)):
+                axs[0].plot(data["StellarAge_BinCenters_Gyr"],
+                            data["[Fe/H]_Median"] \
+                                + facts[i] * data["[Fe/H]_MAD"],
+                            ls=lss[i], c=color, zorder=zorder, lw=1.0)
+
+        for i in range(len(settings.components)):
+            component = settings.components[i]
+            ax = axs[i + 1]
+            if galaxy == 6:
+                color = list(settings.component_colors.values())[i]
+                zorder = 11
+            else:
+                color = "silver"
+                zorder = 10
+
+            ax.plot(data["StellarAge_BinCenters_Gyr"],
+                    data[f"[Fe/H]_Median_{component}"],
+                    ls="-", c=color, zorder=zorder, lw=1.0)
+            if galaxy == 6:
+                for i in range(len(facts)):
+                    ax.plot(
+                        data["StellarAge_BinCenters_Gyr"],
+                        data[f"[Fe/H]_Median_{component}"] \
+                            + facts[i] * data[f"[Fe/H]_MAD_{component}"],
+                        ls=lss[i], c=color, zorder=zorder, lw=1.0)
+
+
+    axs[0].text(
+        x=0.05, y=0.05, size=8.0, ha="left", va="bottom",
+        s=r"$\textbf{All}$", c="black", transform=ax.transAxes)
+    for i in range(len(settings.components)):
+        ax = axs[i + 1]
+        ax.text(
+            x=0.05, y=0.05, size=8.0, ha="left", va="bottom",
+            s=r"$\textbf{" + list(settings.component_labels.values())[i] \
+                + "}$",
+            c=list(settings.component_colors.values())[i],
+            transform=ax.transAxes)
+
+    fig.savefig(
+        f"images/age_metallicity_by_region/"
+        f"sample{config['FILE_SUFFIX']}.pdf")
+    plt.close(fig)
+
+
+def main():
+    figure_setup()
+
+    # Get arguments from user
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    args = parser.parse_args()
+
+    # Load configuration file
+    config = yaml.safe_load(open(f"configs/{args.config}.yml"))
+
+    # Create the plot
+    make_plot(config)
+
+
+if __name__ == "__main__":
+    main()
