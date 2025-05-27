91ee47d7242977f57ab6fed30801e46081e02bd5
Federico Iza <fgiza@protonmail.com>
2025-05-27 14:21:38 +0200

    Add row with un-normalised quantities.

--- scripts/visualization/decomposition_mass_evolution.py
+++ scripts/visualization/decomposition_mass_evolution.py
@@ -1,3 +1,15 @@
+"""
+File:           decomposition_mass_evolution.py
+Description:    Creates figures of the temporal evolution of the mass of the 
+                galaxies and each of their components.
+
+Usage:          python scripts/visualization/decomposition_mass_evolution.py \
+--config CONFIG_FILE
+
+Arguments:
+    --config        Configuration filename for input parameters
+                    (e.g., '02').
+"""
 import numpy as np
 import pandas as pd
 import yaml
@@ -63,23 +75,28 @@ def create_figure_panels_by_component(config: dict):
     settings = Settings()
     SAMPLE = [f"au{i}_or_l4" for i in settings.groups["Included"]]
 
-    fig = plt.figure(figsize=(8.0, 2.0))
-    gs = fig.add_gridspec(nrows=1, ncols=5, hspace=0.0, wspace=0.0)
-    axs = gs.subplots(sharex=True, sharey=True)
+    fig = plt.figure(figsize=(8.0, 3.5))
+    gs = fig.add_gridspec(nrows=2, ncols=5, hspace=0.1, wspace=0.0)
+    axs = gs.subplots(sharex=True, sharey=False)
+
+    axs[0, 0].set_ylabel(r"$M_\star$ [$10^{10} \ \mathrm{M}_\odot$]")
+    axs[1, 0].set_ylabel(r"$M_\star / M_\star (z=0)$")
 
     for ax in axs.flat:
         ax.tick_params(which='both', direction="in")
         ax.set_xlim((0, 14))
-        ax.set_ylim(0, 1)
         ax.set_xticks([2, 4, 6, 8, 10, 12])
-        ax.set_yticks([0, .2, .4, .6, .8, 1])
+        if ax.get_subplotspec().is_first_row():
+            ax.set_ylim(0, 10)
+            ax.set_yticks([1, 3, 5, 7, 9])
+            ax.set_yticklabels(["1.0", "3.0", "5.0", "7.0", "9.0"])
+        else:
+            ax.set_ylim(0, 1)
+            ax.set_yticks([0, .2, .4, .6, .8, 1])
         ax.grid(True, ls='-', lw=0.25, c="gainsboro")
-        ax.set_axisbelow(True)
-        if ax.get_subplotspec().is_last_row() or ax == axs[-2, -1]:
-            ax.set_xlabel("Time [Gyr]")
-            ax.tick_params(labelbottom=True)
-        if ax.get_subplotspec().is_first_col():
-            ax.set_ylabel(r"$M_\star / M_\star (z=0)$")
+        # ax.set_axisbelow(True)
+        ax.set_xlabel("Time [Gyr]")
+        ax.label_outer()
 
     for i, simulation in enumerate(SAMPLE):
         galaxy = parse(simulation)[0]
@@ -92,28 +109,40 @@ def create_figure_panels_by_component(config: dict):
         norm = df["Mass_Msun"].iloc[-1]
 
         #region AllStars
-        ax = axs.flatten()[0]
         color = "silver" if galaxy != HIGHLIGHT else "black"
-        ax.plot(df["Time_Gyr"], df[f"Mass_Msun"] / norm, lw=1.0,
-                color=color, label=label, zorder=zorder)
+        axs[0, 0].plot(
+            df["Time_Gyr"], df[f"Mass_Msun"] / 1E10, lw=1.0,
+            color=color, label=label, zorder=zorder)
+        axs[1, 0].plot(
+            df["Time_Gyr"], df[f"Mass_Msun"].to_numpy() / norm, lw=1.0,
+            color=color, label=label, zorder=zorder)
         #endregion
 
         for j, c in enumerate(settings.components):
-            ax = axs.flatten()[j + 1]
             color = "silver" if galaxy != HIGHLIGHT else \
                 settings.component_colors[c]
-            ax.plot(df["Time_Gyr"], df[f"Mass_{c}_Msun"] / norm, lw=1.0,
-                    color=color, label=label, zorder=zorder)
+            axs[0, j + 1].plot(
+                df["Time_Gyr"], df[f"Mass_{c}_Msun"] / 1E10, lw=1.0,
+                color=color, label=label, zorder=zorder)
+            axs[1, j + 1].plot(
+                df["Time_Gyr"], df[f"Mass_{c}_Msun"].to_numpy() / norm, lw=1.0,
+                color=color, label=label, zorder=zorder)
     
-    axs[0].text(
+    axs[0, 0].text(
         x=0.05, y=0.9, size=7.0, s=r"$\textbf{All}$",
-        ha='left', va='center', transform=axs[0].transAxes)
+        ha='left', va='center', transform=axs[0, 0].transAxes)
+    axs[1, 0].text(
+        x=0.05, y=0.9, size=7.0, s=r"$\textbf{All}$",
+        ha='left', va='center', transform=axs[1, 0].transAxes)
     for j, c in enumerate(settings.components):
-        ax = axs.flatten()[j + 1]
         c_text = settings.component_labels[c]
-        ax.text(
+        axs[0, j + 1].text(
             x=0.05, y=0.9, size=7.0, s=r"$\textbf{" + c_text + "}$",
-            ha='left', va='center', transform=ax.transAxes,
+            ha='left', va='center', transform=axs[0, j + 1].transAxes,
+            color=settings.component_colors[c])
+        axs[1, j + 1].text(
+            x=0.05, y=0.9, size=7.0, s=r"$\textbf{" + c_text + "}$",
+            ha='left', va='center', transform=axs[1, j + 1].transAxes,
             color=settings.component_colors[c])
 
     for ax in axs.flatten():
@@ -137,7 +166,7 @@ def main():
 
     # Create figures
     figure_setup()
-    # create_figure_all_panels(config)
+    create_figure_all_panels(config)
     create_figure_panels_by_component(config)
 
 
